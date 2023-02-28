2426e90bfab9e0b2715a6f793c453c9c821b0140
Felipe Mendoza <felipe.domingo.mendoza@gmail.com>
2023-02-27 23:03:18 -0300

    TypeScript

---
+++ 01-clase-json.js
@@ -0,0 +1,12 @@
+let Bicicleta ={
+color:'Rojo',
+Modelo:'BMX',
+frenos:'De disco',
+VelocidadMax:'60km',
+cambiaColor : function(nuevoColor){
+    this.color = nuevoColor;
+}
+
+}
+
+console.log(Bicicleta)
